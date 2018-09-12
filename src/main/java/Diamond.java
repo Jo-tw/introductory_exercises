4cdc7845ac9361b3270ee65795a94bb0903ef83f
jo-tw <jo.lenis@thoughtworks.com>
2018-09-12 17:40:19 +0200

    Diamond Exercises 4/4 + FizzBuzz

--- src/main/java/Diamond.java
+++ src/main/java/Diamond.java
@@ -1,20 +1,21 @@
 public class Diamond {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         drawTriangle(5);
         drawDiamond(5);
+        drawDiamondWithName(5);
     }
 
 
     static void drawTriangle(int n) {
         System.out.println(" \n\n Draw a isoceles triangle");
 
-        for (int i = 1; i <= n + 2; i+=2) {
+        for (int i = 1; i <= n + 2; i += 2) {
 
-            for (int k = (n + 2 - i)/2; k > 0 ; k--) {
+            for (int k = (n + 2 - i) / 2; k > 0; k--) {
                 System.out.print(' ');
             }
-            for (int j = 1; j <= i ; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print('*');
             }
             System.out.println();
@@ -27,12 +28,12 @@ public class Diamond {
     static void drawDiamond(int n) {
         System.out.println(" \n\n Draw a diamond ");
 
-        for (int i = 1; i <= n + 2; i+=2) {
+        for (int i = 1; i <= n + 2; i += 2) {
 
-            for (int k = (n + 2 - i)/2; k > 0 ; k--) {
+            for (int k = (n + 2 - i) / 2; k > 0; k--) {
                 System.out.print(' ');
             }
-            for (int j = 1; j <= i ; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print('*');
             }
             System.out.println();
@@ -40,12 +41,12 @@ public class Diamond {
 
         }
 
-        for (int i = n; i > 0; i-=2) {
+        for (int i = n; i > 0; i -= 2) {
 
-            for (int k = (n + 2 - i)/2; k > 0 ; k--) {
+            for (int k = (n + 2 - i) / 2; k > 0; k--) {
                 System.out.print(' ');
             }
-            for (int j = 1; j <= i ; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print('*');
             }
             System.out.println();
@@ -54,6 +55,40 @@ public class Diamond {
         }
 
     }
+
+    static void drawDiamondWithName(int n) {
+        System.out.println(" \n\n Draw a diamond ");
+
+        for (int i = 1; i < n + 2; i += 2) {
+
+            for (int k = (n + 2 - i) / 2; k > 0; k--) {
+                System.out.print(' ');
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+
+
+        }
+
+        System.out.println("Jo");
+
+        for (int i = n; i > 0; i -= 2) {
+
+            for (int k = (n + 2 - i) / 2; k > 0; k--) {
+                System.out.print(' ');
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+
+
+        }
+
+    }
+
 
 
 
---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,30 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        doFizzBuzz();
+    }
+
+    static void doFizzBuzz() {
+        System.out.println(" \n\n FizzBuzz");
+
+        for (int i = 1; i <= 100; i += 1) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else {
+
+                if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else {
+
+                    if (i % 5 == 0) {
+                        System.out.println("Buzz");
+                    } else {
+                        System.out.println(i);
+                    }
+                }
+            }
+
+        }
+
+    }
+}
