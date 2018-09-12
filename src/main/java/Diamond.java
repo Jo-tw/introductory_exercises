d69c5d2991b8ada496c689b8c9cde50982064cd5
jo-tw <jo.lenis@thoughtworks.com>
2018-09-12 17:19:46 +0200

    Diamond Exercises 2/4

---
+++ src/main/java/Diamond.java
@@ -0,0 +1,60 @@
+public class Diamond {
+
+    public static void main(String[] args){
+        drawTriangle(5);
+        drawDiamond(5);
+    }
+
+
+    static void drawTriangle(int n) {
+        System.out.println(" \n\n Draw a isoceles triangle");
+
+        for (int i = 1; i <= n + 2; i+=2) {
+
+            for (int k = (n + 2 - i)/2; k > 0 ; k--) {
+                System.out.print(' ');
+            }
+            for (int j = 1; j <= i ; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+
+
+        }
+
+    }
+
+    static void drawDiamond(int n) {
+        System.out.println(" \n\n Draw a diamond ");
+
+        for (int i = 1; i <= n + 2; i+=2) {
+
+            for (int k = (n + 2 - i)/2; k > 0 ; k--) {
+                System.out.print(' ');
+            }
+            for (int j = 1; j <= i ; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+
+
+        }
+
+        for (int i = n; i > 0; i-=2) {
+
+            for (int k = (n + 2 - i)/2; k > 0 ; k--) {
+                System.out.print(' ');
+            }
+            for (int j = 1; j <= i ; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+
+
+        }
+
+    }
+
+
+
+}
