009171a80f6daeba05b3fd0862665342827f858b
Josefina Lenis <jofe@Josefinas-MacBook-Pro.local>
2018-09-12 16:13:50 +0200

    first commit
    Committer: Jo Lenis <jo.lenis@thoughtworks.com>
     On branch master
     Initial commit
     Changes to be committed:
    	new file:   src/main/java/Triangle.java

---
+++ src/main/java/Triangle.java
@@ -0,0 +1,25 @@
+public class Triangle {
+    public static void main(String[] args){
+            easiest();
+            horizontal(5);
+        }
+
+    static void easiest() {
+
+        System.out.println('*');
+
+    }
+
+    static void horizontal(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.print('*');
+
+        }
+
+    }
+
+
+
+}
