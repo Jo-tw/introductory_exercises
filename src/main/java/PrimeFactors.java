cdd07a4b5aeb843d610bf2064e5db0df6a757f78
jo-tw <jo.lenis@thoughtworks.com>
2018-09-13 10:55:11 +0200

    Prime Factors Complete

--- src/main/java/PrimeFactors.java
+++ src/main/java/PrimeFactors.java
@@ -1,2 +1,28 @@
+import java.util.*;
+
 public class PrimeFactors {
+    public static void main(String[] args) {
+    findPrimeFactors(12);
+    }
+
+    static void findPrimeFactors(int n) {
+
+        List<Integer> l1 = new ArrayList<Integer>();
+
+
+        int factor = 2;
+
+        while (n != 1) {
+            if (n % factor == 0) {
+                l1.add(factor);
+                n = n / factor;
+            } else {
+                factor += 1;
+
+            }
+        }
+
+        System.out.println(l1);
+
+    }
 }
